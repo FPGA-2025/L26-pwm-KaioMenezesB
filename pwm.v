d8dd411701f646fdf003770c7a5f2381791011b7
KaioMenezesB <kaio_borduque@hotmail.com>
2025-07-01 08:15:43 -0300

    Atividade finalizada

--- pwm.v
+++ pwm.v
@@ -5,5 +5,21 @@ module PWM (
     input wire [15:0] period, // clk_freq / pwm_freq = period
     output reg pwm_out
 );
-
+    reg [15:0] counter;
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            counter <= 16'd0;
+            pwm_out <= 1'b0;
+        end else begin
+            if (counter < period - 1)
+                counter <= counter + 16'd1;
+            else
+                counter <= 16'd0;
+            // compara contra duty_cycle
+            if (counter < duty_cycle)
+                pwm_out <= 1'b1;
+            else
+                pwm_out <= 1'b0;
+        end
+    end
 endmodule
